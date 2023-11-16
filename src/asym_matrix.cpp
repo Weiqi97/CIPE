5280b96e45d8573e147271f150a85e4d897b1d17
Weiqi <weltch1997@gmail.com>
2023-11-16 00:35:41 -0500

    matrix done

--- src/asym_matrix.cpp
+++ src/asym_matrix.cpp
@@ -1,1 +1,139 @@
 #include "asym_matrix.hpp"
+
+asym::zpMat asym::matrix_zp_from_int(const int *int_mat, int row, int col, point mod) {
+    zpMat x;
+    x = (zpMat) malloc(sizeof(Zp) * row * col);
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            x[i * col + j] = zp_from_int(int_mat[i * col + j], mod);
+        }
+    }
+    return x;
+}
+
+asym::zpMat asym::matrix_zp_rand(int row, int col, point mod) {
+    zpMat x;
+    x = (zpMat) malloc(sizeof(Zp) * row * col);
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            x[i * col + j] = zp_rand(mod);
+        }
+    }
+    return x;
+}
+
+asym::zpMat asym::matrix_identity(int size, point mod) {
+    zpMat x;
+    x = (zpMat) malloc(sizeof(Zp) * size * size);
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (i == j) x[i * size + j] = zp_one(mod);
+            else x[i * size + j] = zp_zero(mod);
+        }
+    }
+    return x;
+}
+
+int asym::matrix_is_identity(zpMat x, int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (i == j && !zp_cmp_int(x[i * size + j], 1)) return 0;
+            if (i != j && !zp_cmp_int(x[i * size + j], 0)) return 0;
+        }
+    }
+    return 1;
+}
+
+asym::zpMat asym::matrix_transpose(zpMat x, int row, int col) {
+    zpMat xt;
+    xt = (zpMat) malloc(sizeof(Zp) * row * col);
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            xt[j * row + i] = zp_copy(x[i * col + j]);
+        }
+    }
+    return xt;
+}
+
+asym::zpMat asym::matrix_merge(zpMat x, zpMat y, int row, int col_x, int col_y) {
+    zpMat xy;
+    xy = (zpMat) malloc(sizeof(Zp) * row * (col_x + col_y));
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col_x; j++) {
+            xy[i * (col_x + col_y) + j] = zp_copy(x[i * col_x + j]);
+        }
+        for (int j = 0; j < col_y; j++) {
+            xy[i * (col_x + col_y) + col_x + j] = zp_copy(y[i * col_y + j]);
+        }
+    }
+    return xy;
+}
+
+asym::zpMat asym::matrix_multiply(zpMat x, zpMat y, int row_x, int row_y, int col_y, point mod) {
+    auto xy = (zpMat) malloc(sizeof(Zp) * row_x * col_y);
+
+    for (int i = 0; i < row_x; i++) {
+        for (int j = 0; j < col_y; j++) {
+            xy[i * row_y + j] = zp_zero(mod);
+            for (int k = 0; k < row_y; k++) {
+                xy[i * col_y + j] = zp_add(xy[i * col_y + j], zp_mul(x[i * row_y + k], y[k * col_y + j]));
+            }
+        }
+    }
+    return xy;
+}
+
+asym::zpMat asym::matrix_inverse(zpMat x, int size, point mod) {
+    // Declare the row echelon matrix and generate it.
+    zpMat identity = matrix_identity(size, mod);
+    zpMat row_echelon = matrix_merge(x, identity, size, size, size);
+
+    // Declare temp value.
+    Zp temp_multiplier;
+    Zp temp_neg;
+
+    // Bottom left half to all zeros.
+    for (int i = 0; i < size; i++) {
+        for (int j = i; j < size; j++) {
+            if (i == j && !zp_cmp_int(row_echelon[i * 2 * size + j], 1)) {
+                temp_multiplier = zp_inv(row_echelon[i * 2 * size + i]);
+                for (int k = i; k < size * 2; k++) {
+                    row_echelon[j * 2 * size + k] = zp_mul(row_echelon[j * 2 * size + k], temp_multiplier);
+                }
+            }
+
+            if (i == j && zp_cmp_int(row_echelon[i * 2 * size + j], 0)) break;
+
+            if (i != j) {
+                temp_multiplier = zp_copy(row_echelon[j * 2 * size + i]);
+                for (int k = i; k < size * 2; k++) {
+                    temp_neg = zp_mul(temp_multiplier, row_echelon[i * 2 * size + k]);
+                    temp_neg = zp_neg(temp_neg);
+                    row_echelon[j * 2 * size + k] = zp_add(row_echelon[j * 2 * size + k], temp_neg);
+                }
+            }
+        }
+    }
+
+    // Top right half to all zeros.
+    for (int i = size - 1; i > 0; i--) {
+        for (int j = i - 1; j >= 0; j--) {
+            temp_multiplier = zp_copy(row_echelon[j * 2 * size + i]);
+            for (int k = i; k < size * 2; k++) {
+                temp_neg = zp_mul(temp_multiplier, row_echelon[i * 2 * size + k]);
+                temp_neg = zp_neg(temp_neg);
+                row_echelon[j * 2 * size + k] = zp_add(row_echelon[j * 2 * size + k], temp_neg);
+            }
+        }
+    }
+
+    // Copy over the output.
+    zpMat xi;
+    xi = (zpMat) malloc(sizeof(Zp) * size * size);
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            xi[i * size + j] = zp_copy(row_echelon[i * 2 * size + size + j]);
+        }
+    }
+    return xi;
+}
--- tests/test_asym_matrix.cpp
+++ tests/test_asym_matrix.cpp
@@ -1,5 +1,68 @@
-//
-// Created by weiqi on 11/16/23.
-//
+#include "asym_matrix.hpp"
 
-#include "test_asym_matrix.h"
+int test_zp_from_int(asym::point N) {
+    int int_mat[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    asym::zpMat x = asym::matrix_zp_from_int(int_mat, 3, 3, N);
+    return asym::zp_cmp_int(x[8], 9);
+}
+
+int test_transpose(asym::point N) {
+    int row = 3, col = 3;
+    asym::zpMat x = asym::matrix_zp_rand(row, col, N);
+    asym::zpMat xt = asym::matrix_transpose(x, row, col);
+    return asym::zp_cmp(xt[col - 1], x[2 * row]);
+}
+
+int test_identity(asym::point N) {
+    int size = 10;
+    asym::zpMat x = asym::matrix_identity(size, N);
+    return asym::matrix_is_identity(x, size);
+}
+
+int test_merge(asym::point N) {
+    int size = 10;
+    asym::zpMat x = asym::matrix_zp_rand(size, size, N);
+    asym::zpMat y = asym::matrix_identity(size, N);
+    asym::zpMat xy = asym::matrix_merge(x, y, size, size, size);
+    return asym::zp_cmp(x[2 * size + 1], xy[4 * size + 1]);
+}
+
+int test_multiply_vector(asym::point N) {
+    int mat_x[5] = {1, 2, 3, 4, 5};
+    int mat_y[15] = {
+            10, 20, 30,
+            10, 20, 30,
+            10, 20, 30,
+            10, 20, 30,
+            10, 20, 30
+    };
+
+    asym::zpMat x = asym::matrix_zp_from_int(mat_x, 1, 5, N);
+    asym::zpMat y = asym::matrix_zp_from_int(mat_y, 5, 3, N);
+    asym::zpMat xy = asym::matrix_multiply(x, y, 1, 5, 3, N);
+    return asym::zp_cmp_int(xy[2], 450);
+}
+
+int test_inverse(asym::point N) {
+    int size = 10;
+    asym::zpMat x = asym::matrix_zp_rand(size, size, N);
+    asym::zpMat xi = asym::matrix_inverse(x, size, N);
+    asym::zpMat r = asym::matrix_multiply(x, xi, size, size, size, N);
+    return asym::matrix_is_identity(r, size);
+}
+
+int main() {
+    // Init core and setup.
+    asym::point N;
+    asym::init_get_order(N);
+
+    // Perform tests.
+    if (test_zp_from_int(N) != 1) return 1;
+    if (test_transpose(N) != 1) return 1;
+    if (test_identity(N) != 1) return 1;
+    if (test_merge(N) != 1) return 1;
+    if (test_multiply_vector(N) != 1) return 1;
+    if (test_inverse(N) != 1) return 1;
+
+    return 0;
+}
