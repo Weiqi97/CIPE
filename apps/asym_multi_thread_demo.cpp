1b7b1b5f20af070c3cb866c69f8e4e8ecba8a920
weiqi <weltch1997@gmail.com>
2023-12-12 11:07:36 -0500

    some touches

--- apps/asym_multi_thread_demo.cpp
+++ apps/asym_multi_thread_demo.cpp
@@ -1,65 +1,54 @@
 #include <chrono>
+#include <random>
 #include <thread>
 #include <vector>
-#include <random>
 #include <iostream>
 #include "asym_ipfe.hpp"
 
+
 int main() {
-    // Seed the random number generator with a random device
+    // Seed the random number generator with a random device and create a distribution.
     std::random_device rd;
     std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> distribution(1, 300);
 
-    // Define the range for the random values
-    int low = 1, up = 300;
-    // Create a distribution for random integers in the specified range
-    std::uniform_int_distribution<int> distribution(low, up);
-
-    // Initialize the scheme.
-    asym::ipfe::Pp ipre_pp = asym::ipfe::ppgen(true, 10, 150);
-    asym::ipfe::Sk ipre_sk = asym::ipfe::setup(ipre_pp);
+    // Set dimensions holders and get the data.
+    int data_dim = 128, data_size = 12000;
 
     // Generate some dummy data.
-    int vector_amount = 5000;
-    int vectors[5000][128];
-    for (auto &vector: vectors) for (auto &i: vector) i = distribution(gen);
+    std::vector<std::vector<int>> vectors(data_size, std::vector<int>(data_dim));
+    for (auto &vector: vectors) for (auto &data: vector) data = distribution(gen);
 
-    // Perform encryption without threading.
-    auto start = std::chrono::high_resolution_clock::now();
-
-    for (auto &vector: vectors) asym::ipfe::enc(ipre_pp, ipre_sk, vector);
-
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
-    std::cout << "Not threaded takes: " << duration.count() << " seconds" << std::endl;
+    // Initialize the scheme.
+    asym::ipfe::Pp ipre_pp = asym::ipfe::ppgen(true, data_dim, 1);
+    asym::ipfe::Sk ipre_sk = asym::ipfe::setup(ipre_pp);
 
     // Declare threads.
     std::vector<std::thread> threads;
     // Get the number of threads.
     auto num_thread = std::thread::hardware_concurrency();
     // Get workload per thread.
-    auto work_per_thread = vector_amount / num_thread;
+    auto work_per_thread = data_size / num_thread;
 
     // Perform encryption with threading.
-    start = std::chrono::high_resolution_clock::now();
-    for (auto i = 0; i < num_thread; ++i) {
+    auto start = std::chrono::high_resolution_clock::now();
+    for (auto i = 0; i < num_thread; i++) {
+        // Set start and end ID.
         auto start_id = i * work_per_thread;
-        auto end_id = (i == num_thread - 1) ? vector_amount : start_id + work_per_thread;
+        auto end_id = (i == num_thread - 1) ? data_size : start_id + work_per_thread;
 
+        // Push work to threads.
         threads.emplace_back([start_id, end_id, ipre_pp, ipre_sk, vectors]() {
-            for (auto i = start_id; i < end_id; i++) {
-                asym::ipfe::enc(ipre_pp, ipre_sk, vectors[i]);
-            }
+            for (auto j = start_id; j < end_id; j++) asym::ipfe::enc(ipre_pp, ipre_sk, vectors[j].data());
         });
     }
 
     // Join threads to the main thread
-    for (int i = 0; i < num_thread; ++i) {
-        threads[i].join();
-    }
-    stop = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
-    std::cout << "Threaded takes: " << duration.count() << " seconds" << std::endl;
+    for (int i = 0; i < num_thread; ++i) threads[i].join();
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
+    std::cout << "The threaded process takes " << duration.count() << " seconds." << std::endl;
 
     return 0;
 }
